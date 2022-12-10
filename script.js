ad7b45a60ba9d9c5fd6d4a5ef69d4f7e19c65170
Nadia <nadia935920@gmail.com>
2022-12-10 16:45:28 +0200

    improved output

--- script.js
+++ script.js
@@ -8,46 +8,38 @@ const sumWithoutCoins =
   Math.floor(priceOfSalt) +
   Math.floor(priceOfPotato);
 
-const discount = Math.floor(Math.random() * 90);
+const discount = Math.floor(Math.random() * 90) + 1;
 
 const amount = Number(prompt('Введіть суму оплати:', 500));
 
 const price = Number(
   prompt('Введіть, будь-ласка, ціну на товар для вичислення прибутку:', 260.66)
 );
-
 const inputDiscount = Number(
   prompt('Введіть знижку на товар для обчислення прибутку:', 10)
 );
 
 confirm('Запустіть консоль (F12)');
 
-console.log(`Максимальна ціна: ${Math.max(...arr)} грн.`);
-console.log(`Мінімальна ціна: ${Math.min(...arr)} грн. `);
-console.log(`Вартість всіх товарів: ${sum} грн.`);
-console.log(`Вартість всіх товарів без копійок: ${sumWithoutCoins} грн.`);
-console.log(
-  `Сума товару в сотнях: ${
-    Math.floor(sum) % 100 >= 50
-      ? Math.floor(sum) - (Math.floor(sum) % 100) + 100
-      : Math.floor(sum) - (Math.floor(sum) % 100)
-  }`
-);
-console.log(
-  `Сума всіх товарів парна? ${sumWithoutCoins % 2 == 0 ? true : false}`
-);
-console.log(
-  `Ви заплатили - ${amount}. Ваша решта складає: ${amount - sum} грн.`
-);
-console.log(`Середнє значення цін: ${(sum / 3).toFixed(2)} грн.`);
-console.log(
-  `Сума до оплати враховуючи знижку ${discount}% складає ${(
-    sum -
-    sum * (discount / 100)
-  ).toFixed(2)} грн.`
-);
-console.log(
-  `Знижка на товар складає ${inputDiscount}%. Ціна на товару - ${price}. Прибуток за товар - ${Math.round(
-    price / 2 - price * (inputDiscount / 100)
-  )} грн.`
-);
+const evenSum = sumWithoutCoins % 2 == 0;
+
+console.log(`Максимальна ціна: ${Math.max(...arr)} грн.
+Мінімальна ціна: ${Math.min(...arr)} грн.
+Вартість всіх товарів: ${sum} грн.
+Вартість всіх товарів без копійок: ${sumWithoutCoins} грн.
+Сума товару в сотнях: ${
+  Math.floor(sum) % 100 >= 50
+    ? Math.floor(sum) - (Math.floor(sum) % 100) + 100
+    : Math.floor(sum) - (Math.floor(sum) % 100)
+}
+Сума всіх товарів парна? ${evenSum ? evenSum : evenSum}
+Ви заплатили - ${amount}. Ваша решта складає: ${amount - sum} грн.
+Середнє значення цін: ${(sum / 3).toFixed(2)} грн.
+Сума до оплати враховуючи знижку ${discount}% складає ${(
+  sum -
+  sum * (discount / 100)
+).toFixed(2)} грн.
+Знижка на товар складає ${inputDiscount}%. Ціна товару - ${price}. Прибуток за товар - ${Math.round(
+  price / 2 - price * (inputDiscount / 100)
+)} грн.
+`);
