32b1bcb4e046a9ede573b71fb7e2d9c11e59d564
Nadia <nadia935920@gmail.com>
2022-12-13 14:36:49 +0200

    refactoring and reorganization with some changes

--- homework_02/script.js
+++ homework_02/script.js
@@ -1,24 +1,49 @@
-const n = inputNumber('N, де N - початок діапазону');
-const m = inputNumber('M, де M - кінець діапазону');
-const skipEven = confirm('Пропускати парні числа?');
+main();
 
-function inputNumber(name) {
-  return checkNumber(showInputPrompt(name), name);
+function main() {
+  let n = showInputPrompt('N, де N - початок діапазону');
+  if (n == null) {
+    alert('Обчислити додавання неможливо!');
+    return;
+  }
+
+  let m = showInputPrompt('M, де M - кінець діапазону');
+  if (m == null) {
+    alert('Обчислити додавання неможливо!');
+    return;
+  }
+
+  const skipEven = confirm('Пропускати парні числа?');
+  const sum = sumRange(n, m, skipEven);
+  alert(`Результатом складання чисел від ${n} до ${m} є ${sum}`);
+}
+
+function confirmCancelInput(name) {
+  const skipConfirm = confirm('Ви дійсно хочете скасувати введення числа?');
+  if (!skipConfirm) {
+    return showInputPrompt(name);
+  }
+  return null;
 }
 
 function showInputPrompt(name) {
-  return parseInt(prompt(`Введіть,будь-ласка, ${name}`), 10);
+  const userInput = parseInt(prompt(`Введіть,будь-ласка ${name}`), 10);
+  if (isNaN(userInput)) {
+    return confirmCancelInput(name);
+  }
+
+  return repeatInput(userInput, name);
 }
 
-function checkNumber(num, name) {
-  while (isNaN(num) || num < 1) {
+function repeatInput(num, name) {
+  while (num < 1) {
     alert('Будь-ласка,введіть дійсне число');
     num = showInputPrompt(name);
   }
   return num;
 }
 
-function sumRange(from, to) {
+function sumRange(from, to, skipEven) {
   let sum = 0;
   for (let i = from; i <= to; i++) {
     if (!skipEven || i % 2 !== 0) {
@@ -27,5 +52,3 @@ function sumRange(from, to) {
   }
   return sum;
 }
-
-alert(`Результатом складання чисел від ${n} до ${m} є ${sumRange(n, m)}`);
