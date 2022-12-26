064e87b128063adf8ff50678ed8bcad378f4b5f5
Nadia <nadia935920@gmail.com>
2022-12-26 22:32:27 +0200

    objects hw done

--- homework_06/scripts/main.js
+++ homework_06/scripts/main.js
@@ -1,237 +1,5 @@
-// 'use strict';
-// import {
-//   getRandomArray,
-//   getModa,
-//   getAverage,
-//   getMedian,
-//   filterEvenNumbers,
-//   getDividedByFive,
-//   countPositiveNumber,
-//   replaceBadWords,
-//   dividedByThree,
-//   getIntegerFromArray,
-// } from './functions.js';
-// //output
-// const list = [
-//   {
-//     content: 'Функція №1 (Генерування масиву випадкових цілих чисел).',
-//     funcName: 'GetRandomArray',
-//     action: getRandomArray,
-//     validation: ['integer', 'integer', 'integer'],
-//     placeholder: ['len', 'min', 'max'],
-//     'type of output': '[]',
-//   },
-//   {
-//     content: 'Функція №2 (Функція №2 (Визначення моди масиву).',
-//     funcName: 'GetModa',
-//     action: getModa,
-//     validation: ['number'],
-//     placeholder: ['1,2,3,6'],
-//     'type of output': 'number',
-//   },
-//   {
-//     content: 'Функція №3 (Середнє арифметичне всіх чисел).',
-//     funcName: 'GetAverage',
-//     action: getAverage,
-//     validation: ['number'],
-//     placeholder: ['1,2,3,6'],
-//     'type of output': 'number',
-//   },
-//   {
-//     content: 'Функція №4(Обчислнння медіани всіх чисел).',
-//     funcName: 'GetMedian',
-//     action: getMedian,
-//     validation: ['number'],
-//     placeholder: ['1,2,3,6'],
-//     'type of output': 'number',
-//   },
-//   {
-//     content: 'Функція №5(Фільтрація парних чисел).',
-//     funcName: 'FilterEvenNumbers',
-//     action: filterEvenNumbers,
-//     validation: ['number'],
-//     placeholder: ['1,2,3,6'],
-//     'type of output': '[]',
-//   },
-//   {
-//     content: 'Функція №6(Виводить числа, що діляться на 5 ).',
-//     funcName: 'GetDividedByFive',
-//     action: getDividedByFive,
-//     validation: ['number'],
-//     placeholder: ['1,2,3,6'],
-//     'type of output': '[]',
-//   },
-//   {
-//     content: 'Функція №7(Виводить кількість позитивних чисел).',
-//     funcName: 'СountPositiveNumber',
-//     action: countPositiveNumber,
-//     validation: ['number'],
-//     placeholder: ['1,2,3,6'],
-//     'type of output': 'number',
-//   },
-//   {
-//     content: 'Функція №8(Цензура)',
-//     funcName: 'ReplaceBadWords',
-//     action: replaceBadWords,
-//     validation: ['string'],
-//     placeholder: ['Are you fucking kidding?'],
-//     'type of output': 'string',
-//   },
-//   {
-//     content: 'Функція №9(Розбиття слова на 3-ох значні склади).',
-//     funcName: 'DividedByThree',
-//     action: dividedByThree,
-//     placeholder: ['word'],
-//     validation: ['string'],
-//     'type of output': '[]',
-//   },
-// ];
-// const wrapper = document.querySelector('.wrapper');
+'use strict';
 
-// function showPopup(content, x, y) {
-//   const exist = document.querySelector('.popup');
-//   if (!exist) {
-//     const d = document.createElement('div');
-//     d.classList.add('popup');
-//     d.innerHTML = content;
-//     if (typeof x !== 'undefined' && typeof y !== 'undefined') {
-//       d.style.display = 'absolute';
-//       d.style.left = x + 'px';
-//       d.style.top = y + 'px';
-//     }
-//     document.body.appendChild(d);
-//     setTimeout(() => {
-//       document.body.removeChild(d);
-//     }, 1000);
-//   }
-// }
-
-// function createElement(item) {
-//   const template = document
-//     .getElementById('item-template')
-//     .content.cloneNode(true);
-
-//   const title = template.querySelector('.content');
-//   title.textContent = item.content;
-
-//   const funcName = template.querySelector('.func_name');
-//   funcName.textContent = item.funcName;
-//   const dyn = template.querySelector('.dynamic');
-
-//   const icon = template.querySelector('.st-icon');
-//   icon.dataset.code = encodeURIComponent(item.action.toString());
-
-//   icon.addEventListener('click', function () {
-//     const code = decodeURIComponent(this.dataset.code);
-//     const codeInfo = document.querySelector('.info-code');
-//     codeInfo.textContent = code;
-//   });
-
-//   for (let i = 0; i < item.action.length; ++i) {
-//     const template = document
-//       .getElementById('text-input')
-//       .content.cloneNode(true);
-//     const inp = template.querySelector('input');
-//     inp.setAttribute('placeholder', `${item.placeholder[i]}`);
-//     dyn.appendChild(template);
-//   }
-
-//   const submit = template.querySelector('[type=submit]');
-//   const output = template.querySelector('.st-output');
-
-//   submit.addEventListener('click', (e) => {
-//     e.preventDefault();
-//     const inputs = [...dyn.querySelectorAll('input')];
-
-//     const values = inputs.map((input, i) => {
-//       switch (item.validation[i]) {
-//         case 'integer': {
-//           if (!/^[0-9]|\,$/.test(input.value)) {
-//             showPopup(
-//               `Invalid integer`,
-//               input.offsetLeft,
-//               input.offsetTop + input.offsetHeight
-//             );
-//           }
-//           return parseInt(input.value, 10);
-//         }
-//         case 'string': {
-//           if (!/([a-zA-Zа-яА-ЯіІ]|\?)/gi.test(input.value)) {
-//             showPopup(
-//               `Invalid string`,
-//               input.offsetLeft,
-//               input.offsetTop + input.offsetHeight
-//             );
-//           }
-//           return input.value;
-//         }
-//         case 'number': {
-//           if (!/^[+-]|[0-9]|\,$/.test(input.value)) {
-//             showPopup(
-//               `invalid number`,
-//               input.offsetLeft,
-//               input.offsetTop + input.offsetHeight
-//             );
-//           }
-//           return input.value.split(',').map((el) => Number(el));
-//         }
-//         default:
-//           return input.value;
-//       }
-//     });
-//     const result = item.action(...values);
-
-//     item['type of output'] == 'number' || item['type of output'] == 'string'
-//       ? (output.textContent = `${String(result)}`)
-//       : (output.textContent = `[${String(result)}]`);
-//     inputs.map((input) => {
-//       if (
-//         input.value == '' ||
-//         (/^[0-9]/gi.test(input.value) && item.validation == 'string') ||
-//         (/^([a-zA-Z]|[а-яА-ЯіІ]|\?)/gi && item.validation == 'number') ||
-//         (/^([a-zA-Z]|[а-яА-ЯіІ]|\?)/gi && item.validation == 'integer') ||
-//         (input.value = '' && item.validation == 'integer')
-//       ) {
-//         output.textContent = '';
-//       }
-//     });
-//   });
-
-//   return template;
-// }
-
-// for (let item of list) {
-//   wrapper.appendChild(createElement(item));
-// }
-
-// const btnShowModal = document.querySelectorAll('.show-modal');
-// const modal = document.querySelector('.modal');
-// const overlay = document.querySelector('.overlay');
-// const btnCloseModal = document.querySelector('.close-modal');
-
-// const closeModal = function () {
-//   modal.classList.add('hidden');
-//   overlay.classList.add('hidden');
-// };
-
-// const openModal = function () {
-//   // codeContext.textContent = this.dataset.code;
-//   modal.classList.remove('hidden');
-//   overlay.classList.remove('hidden');
-// };
-
-// for (let i = 0; i < btnShowModal.length; i++) {
-//   btnShowModal[i].addEventListener('click', openModal);
-// }
-
-// btnCloseModal.addEventListener('click', closeModal);
-// overlay.addEventListener('click', closeModal);
-
-// document.addEventListener('keydown', function (e) {
-//   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-//     closeModal();
-//   }
-// });
 const students = [
   {
     name: 'Tanya',
@@ -262,30 +30,13 @@ const students = [
   },
 ];
 
-// Створіть функцію getSubjects(students[0] --> ["Math", "Algorithms", "Data science"]
-// - яка повертає список предметів для конкретного студента. Зверніть увагу –
-// назву предмету необхідно повертати з великої літери, а _ – замінити на пробіл
-// Створіть функцію getAverageMark(students[0]) --> 3.79 – яка поверне середню
-// оцінку по усім предметам для переданого студента НЕ МАСИВА СТУДЕНТІВ.
-// Оцінку округліть до 2ого знаку. Можна використовувати функції, написані у
-// попередніх домашніх завданнях :)
-// Створіть функцію getStudentInfo(students[0]) --> { "course": 3, "name": "Tanya",
-// "averageMark": 3.79} – яка повертає інформацію загального виду по переданому
-// студенту (вам знадобиться функція з попереднього завдання). ПОвинна бути
-// виведена інформація: курс, ім'я, середня оцінка.
-// Ствроіть функцію getStudentsNames(students) --> ["Anton", "Tanya, "Victor"] – яка
-// повертає імена студентів у алфавітному порядку.
-// Створіть функцію getBestStudent(students) --> "Anton" – яка повертає кращого
-// студента зі списку по показнику середньої оцінки.
-// Cтворіть функцію calculateWordLetters("тест") --> { "т": 2, "е": 1, "с": 1 } – яка
-// повертає обє'кт, в якому ключі це букви у слові, а значення – кількість їх
-// повторень.
-
 function getSubjects(item) {
   const subjects = Object.keys(item.subjects);
-  return subjects.map((el) => {
-    return firstLetterUpCase(el).replace('_', ' ');
-  });
+  return subjects
+    .map((el) => {
+      return firstLetterUpCase(el).replace('_', ' ');
+    })
+    .join(', ');
 }
 
 function firstLetterUpCase(str) {
@@ -293,6 +44,14 @@ function firstLetterUpCase(str) {
 }
 
 function getAverageMark(item) {
+  // function getAverage(arr) {
+  //   arr = [...arr];
+  //   return (
+  //     arr.reduce((acc, el) => {
+  //       return acc + el;
+  //     }) / arr.length
+  //   ).toFixed(2);
+  // }
   const marks = Object.values(item.subjects);
   return getAverage(marks.map((arr) => parseFloat(getAverage(arr), 10)));
 }
@@ -306,24 +65,41 @@ function getAverage(arr) {
   ).toFixed(2);
 }
 
+//Ця функція для виведення результату на сторінку. По суті, функція працює лише return rest, але на виводі буде об'єкт, нічого кращо я не придумала))
+function parseObjectInArr(obj) {
+  const res = [];
+  Object.entries(obj).map((el) => {
+    console.log();
+    res.push(`"${el[0]}": ${el[1]}`);
+  });
+  return res;
+}
+
 function getStudentInfo(student) {
   student.averageMark = getAverageMark(student);
   const { subjects, ...rest } = student;
   return rest;
 }
+
+function outputGetStudentInfo(student) {
+  student.averageMark = getAverageMark(student);
+  const { subjects, ...rest } = student;
+  return parseObjectInArr(rest);
+}
+
 function getStudentsName(students) {
   const names = students.map((el) => {
     const { name, ...rest } = el;
     return name;
   });
-  return names.sort((a, b) => {
-    if (a < b) return -1;
-    if (a > b) return 1;
-    return 0;
-  });
+  return names
+    .sort((a, b) => {
+      if (a < b) return -1;
+      if (a > b) return 1;
+      return 0;
+    })
+    .join(', ');
 }
-
-console.log(getStudentsName(students));
 
 function getBestStudent(student) {
   const resultsOfStudents = student.map((el) => {
@@ -341,7 +117,216 @@ function getBestStudent(student) {
     },
     { name: '', maxAgv: 0 }
   );
-
   return res.name;
 }
-console.log(getBestStudent(students));
+
+function calculateWordLetters(word) {
+  const str = word.toLowerCase().replaceAll(/\s/gi, '');
+  const res = {};
+  for (let ch of str) {
+    if (res.hasOwnProperty(ch)) {
+      ++res[ch];
+    } else {
+      res[ch] = 1;
+    }
+  }
+  return parseObjectInArr(res); //можна обійтись просто return res, але для виводу функції треба запарсити в масив
+}
+
+const list = [
+  {
+    content: 'Функція №1 (Повертає список предметів для конкретного студента).',
+    funcName: 'GetSubjects',
+    action: getSubjects,
+    placeholder: ['studets[0] ([1] чи [2])'],
+    validation: ['expression'],
+    info: getSubjects,
+  },
+  {
+    content: 'Функція №2 (Виводить середню оцінку студента по всіх предметах).',
+    funcName: 'GetAverageMark',
+    action: getAverageMark,
+    placeholder: ['studets[0] ([1] чи [2])'],
+    validation: ['expression'],
+    info: getAverageMark,
+  },
+  {
+    content:
+      "Функція №3 (Повертає інформацію загального вигляду ([курс, ім'я, середня оцінка]) ).",
+    funcName: 'GetStudentInfo',
+    action: outputGetStudentInfo,
+    placeholder: ['studets[0] ([1] чи [2])'],
+    validation: ['expression'],
+    info: getStudentInfo,
+  },
+  {
+    content: 'Функція №4 (Повертає імена студентів в алфавітному порядку).',
+    funcName: 'GetStudentsName',
+    action: getStudentsName,
+    placeholder: ['students'],
+    validation: ['expression'],
+    info: getStudentsName,
+  },
+  {
+    content:
+      'Функція №5 (Повертає кращого студента зі списку по показнику середньої оцінки).',
+    funcName: 'GetBestStudent',
+    action: getBestStudent,
+    placeholder: ['students'],
+    validation: ['expression'],
+    info: getBestStudent,
+  },
+  {
+    content:
+      "Функція №6 (Повертає обє'кт, в якому ключь - букви у слові, а значення – кількість їх повторень).",
+    funcName: 'CalculateWordLetters',
+    action: calculateWordLetters,
+    placeholder: ['word'],
+    validation: ['string'],
+    info: calculateWordLetters,
+  },
+];
+
+function showPopup(content, x, y) {
+  const exist = document.querySelector('.popup');
+  if (!exist) {
+    const d = document.createElement('div');
+    d.classList.add('popup');
+    d.innerHTML = content;
+    if (typeof x !== 'undefined' && typeof y !== 'undefined') {
+      d.style.display = 'absolute';
+      d.style.left = x + 'px';
+      d.style.top = y + 'px';
+    }
+    document.body.appendChild(d);
+    setTimeout(() => {
+      document.body.removeChild(d);
+    }, 1000);
+  }
+}
+
+const wrapper = document.querySelector('.wrapper');
+
+function createElement(item) {
+  const template = document
+    .getElementById('item-template')
+    .content.cloneNode(true);
+  const title = template.querySelector('.content');
+  title.textContent = item.content;
+
+  const funcName = template.querySelector('.func_name');
+  funcName.textContent = item.funcName;
+  const dyn = template.querySelector('.dynamic');
+
+  const icon = template.querySelector('.st-icon');
+  icon.dataset.code = encodeURIComponent(item.info.toString());
+
+  icon.addEventListener('click', function () {
+    const code = decodeURIComponent(this.dataset.code);
+    const codeInfo = document.querySelector('.info-code');
+    codeInfo.textContent = code;
+  });
+
+  for (let i = 0; i < item.action.length; ++i) {
+    const template = document
+      .getElementById('text-input')
+      .content.cloneNode(true);
+    const inp = template.querySelector('input');
+    inp.setAttribute('placeholder', `${item.placeholder[i]}`);
+    dyn.appendChild(template);
+  }
+
+  const submit = template.querySelector('[type=submit]');
+  const output = template.querySelector('.st-output');
+
+  submit.addEventListener('click', (e) => {
+    e.preventDefault();
+    const inputs = [...dyn.querySelectorAll('input')];
+
+    const values = inputs.map((input, i) => {
+      switch (item.validation[i]) {
+        case 'expression':
+          if (!/[students]\[[0-2]\] |[students]/g.test(input.value)) {
+            showPopup(
+              `Please, enter velue as in a placeholder's`,
+              input.offsetLeft,
+              input.offsetTop + input.offsetHeight
+            );
+            return;
+          }
+          try {
+            const result = new Function('students', `return ${input.value}`);
+            return result(students);
+          } catch (error) {
+            showPopup(
+              `${error}`,
+              input.offsetLeft,
+              input.offsetTop + input.offsetHeight
+            );
+          }
+
+        case 'string': {
+          if (!/([a-zA-Zа-яА-ЯіІ])/gi.test(input.value)) {
+            showPopup(
+              `Invalid string`,
+              input.offsetLeft,
+              input.offsetTop + input.offsetHeight
+            );
+            return output.classList.add('hidden');
+          }
+          return input.value;
+        }
+        default:
+          return input.value;
+      }
+    });
+    try {
+      const result = item.action(...values);
+      output.classList.remove('hidden');
+      output.innerHTML = result;
+    } catch (error) {
+      inputs.map((input) => {
+        showPopup(
+          `${error}`,
+          input.offsetLeft,
+          input.offsetTop + input.offsetHeight
+        );
+      });
+    }
+  });
+
+  return template;
+}
+
+for (let item of list) {
+  wrapper.appendChild(createElement(item));
+}
+
+const btnShowModal = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const openModal = function () {
+  // codeContext.textContent = this.dataset.code;
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < btnShowModal.length; i++) {
+  btnShowModal[i].addEventListener('click', openModal);
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
