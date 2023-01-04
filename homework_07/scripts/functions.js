20530bab5cfb9a9f618c3b2b807ad351d0c2370b
Nadia <nadia935920@gmail.com>
2023-01-04 22:15:13 +0200

    improve code

--- homework_07/scripts/functions.js
+++ homework_07/scripts/functions.js
@@ -18,3 +18,30 @@ export function getMySalary(country) {
     profit: randomSalary - this.tax * randomSalary,
   };
 }
+
+export function showOutput(arr, salary) {
+  if (salary) {
+    return arr.map((el) => {
+      return console.log(
+        `%c getMyTaxes.call(${el[0]}, ${salary}) - %c${el[1]}`,
+        "font-style: italic",
+        "font-weight: bold"
+      );
+    });
+  }
+  return arr.map((el) => {
+    return console.log(
+      `%c getMyTaxes.call(${el[0]}) - %c${el[1]}`,
+      "font-style: italic",
+      "font-weight: bold"
+    );
+  });
+}
+
+export function createArray(ukr, lat, lit) {
+  return [
+    ["Ukraine", `${ukr}`],
+    ["Latvia", `${lat}`],
+    ["Litva", `${lit}`],
+  ];
+}
--- homework_07/scripts/script.js
+++ homework_07/scripts/script.js
@@ -1,12 +1,13 @@
+("use strict");
+alert("Press F12");
 import {
   getMyTaxes,
   getMiddleTaxes,
   getTotalTaxes,
   getMySalary,
+  showOutput,
+  createArray,
 } from "./functions.js";
-
-("use strict");
-alert("Press F12");
 
 const ukraine = {
   tax: 0.195,
@@ -30,105 +31,52 @@ const salary = parseInt(prompt("Please, enter salary", 1000));
 const ukrTaxes = getMyTaxes.call(ukraine, salary);
 const latTaxes = getMyTaxes.call(latvia, salary);
 const litTaxes = getMyTaxes.call(litva, salary);
+const arrayTaxes = createArray(ukrTaxes, latTaxes, litTaxes);
 
 console.group("Calculation salary by countries");
-console.log(
-  `%c getMyTaxes.call(Ukraine, ${salary}) - %c${ukrTaxes}`,
-  "font-style: italic",
-  "font-weight: bold"
-);
-console.log(
-  `%c getMyTaxes.call(Latvia, ${salary}) - %c${latTaxes}`,
-  "font-style: italic",
-  "font-weight: bold"
-);
-console.log(
-  `%c getMyTaxes.call(Litva, ${salary}) - %c${litTaxes}`,
-  "font-style: italic",
-  "font-weight: bold"
-);
+showOutput(arrayTaxes, salary);
 console.groupEnd();
 
 const ukrMiddlTaxes = getMiddleTaxes.call(ukraine);
 const latMiddlTaxes = getMiddleTaxes.call(latvia);
 const litMiddlTaxes = getMiddleTaxes.call(litva);
+const arrayMiddleTaxes = createArray(
+  ukrMiddlTaxes,
+  latMiddlTaxes,
+  litMiddlTaxes
+);
 
 console.group("Calculation middle salary by countries");
-console.log(
-  `%c getMyTaxes.call(Ukraine) - %c${ukrMiddlTaxes}`,
-  "font-style: italic",
-  "font-weight: bold"
-);
-console.log(
-  `%c getMyTaxes.call(Latvia) - %c${latMiddlTaxes}`,
-  "font-style: italic",
-  "font-weight: bold"
-);
-console.log(
-  `%c getMyTaxes.call(Litva) - %c${litMiddlTaxes}`,
-  "font-style: italic",
-  "font-weight: bold"
-);
+showOutput(arrayMiddleTaxes);
 console.groupEnd();
 
 const ukrTotalTaxes = getTotalTaxes.call(ukraine);
 const latTotalTaxes = getTotalTaxes.call(latvia);
 const litTotalTaxes = getTotalTaxes.call(litva);
+const arrayTotalTaxes = createArray(
+  ukrTotalTaxes,
+  latTotalTaxes,
+  litTotalTaxes
+);
 
 console.group("Calculation total taxes by countries");
-console.log(
-  `%c getMyTaxes.call(Ukraine) - %c${ukrTotalTaxes}`,
-  "font-style: italic",
-  "font-weight: bold"
-);
-console.log(
-  `%c getMyTaxes.call(Latvia) - %c${latTotalTaxes}`,
-  "font-style: italic",
-  "font-weight: bold"
-);
-console.log(
-  `%c getMyTaxes.call(Litva) - %c${litTotalTaxes}`,
-  "font-style: italic",
-  "font-weight: bold"
-);
+showOutput(arrayTotalTaxes);
 console.groupEnd();
 
 console.group("Calculation total taxes by countries");
+
 (function repeat() {
   const mySalaryInUkr = JSON.stringify(getMySalary.call(ukraine));
-  setTimeout(function () {
-    console.log(
-      `%c getMyTaxes.call(Ukraine) - %c${mySalaryInUkr}`,
-      "font-style: italic",
-      "font-weight: bold"
-    );
-    repeat();
-  }, 5000);
-})();
-
-(function repeat() {
   const mySalaryInLat = JSON.stringify(getMySalary.call(latvia));
-  setTimeout(function () {
-    console.log(
-      `%c getMyTaxes.call(Latvia) - %c${mySalaryInLat}`,
-      "font-style: italic",
-      "font-weight: bold"
-    );
-    repeat();
-  }, 5000);
-})();
-
-(function repeat() {
   const mySalaryInLit = JSON.stringify(getMySalary.call(litva));
+  const arraymySalary = createArray(
+    mySalaryInUkr,
+    mySalaryInLat,
+    mySalaryInLit
+  );
   setTimeout(function () {
-    console.log(
-      `%c getMyTaxes.call(Litva) - %c${mySalaryInLit}`,
-      "font-style: italic",
-      "font-weight: bold",
-      "\n\n"
-    );
+    showOutput(arraymySalary);
     repeat();
   }, 5000);
 })();
-
 console.groupEnd();
